2c2f934654b13c95a43153c439b8b94cf67d833f
edward11231 <tavarez92@gmail.com>
2019-10-05 20:51:33 -0400

    Assignment Done

---
+++ Sound and Animation Tutorial/Assets/Scripts/CatController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public AudioClip audioClip1;
+    public AudioClip audioClip2;
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = audioClip1;
+            musicSource.Play();
+            animator.SetInteger("State", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+            animator.SetInteger("State", 0);
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = audioClip2;
+            musicSource.Play();
+            animator.SetInteger("State", 2);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            animator.SetInteger("State", 0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("Game Quit");
+        }
+
+    }
+}
